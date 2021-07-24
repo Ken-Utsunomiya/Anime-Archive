fdbeccc83a5ebf3059a0552d5a3631f186d0ee8e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-07-23 22:32:29 -0700

    feat: implement card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,38 @@
+import React from 'react';
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
-const Card = ({ id, title }) => {
+import no_image from '../assets/images/no_image.jpg'
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    height: 270
+  },
+  media: {
+    height: 200,
+  },
+});
+
+const WorkCard = ({ id, title, image_url }) => {
+  const classes = useStyles();
+  const image = image_url ? image_url : no_image
+
   return (
-    <div className='item' key={id}>
-      <Link to={`/works/${id}`} >{title}</Link>
-    </div>
-  )
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Link to={`/works/${id}`} >{title}</Link>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Card
+export default WorkCard
